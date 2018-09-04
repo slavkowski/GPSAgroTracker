a49c062f15952d7e1d95b851abc076af583fa4b3
Slaptop\Slavkowski <slawomir.szwed@vsf-experts.pl>
2018-09-04 19:54:18 +0200

    add Gps Serial Communication

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -12,13 +12,12 @@ import javafx.scene.paint.Color;
 import javafx.event.ActionEvent;
 
 
-
+import java.io.IOException;
 import java.net.URL;
 
 
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.ResourceBundle;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Controller implements Initializable {
     int initialWidth = 600;
@@ -26,10 +25,8 @@ public class Controller implements Initializable {
     WindowDimension windowDimension = new WindowDimension(initialWidth, initialHeight);
 
 
-
     private Enumeration ports = null;
     private HashMap portMap = new HashMap();
-
 
 
     @FXML
@@ -58,9 +55,10 @@ public class Controller implements Initializable {
 
         gpMap.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> resizeMapCanvas(windowDimension.getWindowWidth(newSceneWidth.intValue()), windowDimension.getWindowHeight()));
         gpMap.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> resizeMapCanvas(windowDimension.getWindowWidth(), windowDimension.getWindowHeight(newSceneHeight.intValue())));
+
     }
 
-    public void resizeMapCanvas(int newWidth, int newHeight){
+    public void resizeMapCanvas(int newWidth, int newHeight) {
         GraphicsContext gc;
         gc = mapCanvas.getGraphicsContext2D();
 //        System.out.println(newWidth + " " + newHeight);
@@ -69,7 +67,7 @@ public class Controller implements Initializable {
         gc.setFill(Color.GREY);
         gc.fillRect(0, 0, newWidth, newHeight);
 
-        gc.strokeOval(newWidth/2-1,newHeight/2-1,2,2);
+        gc.strokeOval(newWidth / 2 - 1, newHeight / 2 - 1, 2, 2);
 
     }
 
@@ -90,6 +88,86 @@ public class Controller implements Initializable {
 //        window.txtLog.append(logText + "\n");
     }
 
-//    public void getSize(javafx.event.ActionEvent actionEvent) {
-//    }
+    public void drawLine() {
+        GraphicsContext gc;
+        gc = mapCanvas.getGraphicsContext2D();
+
+    }
+
+    @FXML
+    public void readSerialGps(ActionEvent actionEvent) {
+        GpsSerialConnection gpsSerialConnection = new GpsSerialConnection();
+        GpsSerialConnection rxtx = new GpsSerialConnection();
+
+        try {
+            rxtx.connect("COM5");
+
+            //Define a working thread to read RxTx and display in the console
+            Thread readData = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String data;
+                    while(true) {
+                        if (Thread.interrupted()) break;
+                        data = rxtx.read();
+                        if (data != null) {
+                            System.out.println(data);
+                        } else {
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            });
+            readData.start();
+
+            //Define a input thread to get input from the user and send to RxTx
+            Thread userInput = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int c = 0;
+                    byte[] buf = new byte[1204];
+                    try {
+                        while ( ( c = System.in.read(buf)) > -1 )
+                        {
+                            if (Thread.interrupted()) break;
+                            //"---" is a string to terminate the program
+                            if (c > 0) {
+                                if (c==5 && buf[0]=='-'
+                                        && buf[1]=='-' && buf[2]=='-') {
+                                    break;
+                                }
+                                //"+++" is a special string sent to RxTx
+                                if (c==5 && buf[0]=='+'
+                                        && buf[1]=='+' && buf[2]=='+') {
+                                    c = 3;
+                                }
+                                rxtx.write(Arrays.copyOf(buf, c));
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            userInput.start();
+
+            //Normally the program is terminated by the user
+            //so we wait for the input thread to finish
+            userInput.join();
+            readData.interrupt();
+            readData.join();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //close RxTx to release all resources
+            rxtx.close();
+            System.out.println("Finished!");
+        }
+    }
 }
+
